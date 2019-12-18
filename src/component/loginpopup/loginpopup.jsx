2341db3dcfdf4acec9fd7c4e97f41480cc981ee0
sumit3021kumar <manudogra3021@gmail.com>
2019-12-18 17:25:43 +0530

    added signup form

--- src/component/loginpopup/loginpopup.jsx
+++ src/component/loginpopup/loginpopup.jsx
@@ -3,6 +3,7 @@ import {
 	withRouter
 } from 'react-router-dom';
 import Input from './../input/input.jsx';
+import SignUp from './../signup/signup.jsx';
 import './loginpopup.css';
 
 class LoginPopUP extends React.Component {
@@ -16,7 +17,9 @@ class LoginPopUP extends React.Component {
          valueEmail: '',
          valuePass: '',
 				 error: '',
-				 errorClass: 'error'
+				 errorClass: 'error',
+				 signup : false,
+				 signuptext: "Sign Up"
       };
     this.handleChangeLogin = this.handleChangeLogin.bind(this);
     this.handleChangeEmail = this.handleChangeEmail.bind(this);
@@ -38,16 +41,40 @@ class LoginPopUP extends React.Component {
      e.preventDefault();
      let email = this.state.valueEmail;
      let pass = this.state.valuePass;
-		 if ( (email === 'test@gmail.com') && (pass === '123')) {
-			 var arr = {'token': true, 'useremail' : email , 'password' : pass};
-       localStorage.setItem('LoginDetail', JSON.stringify(arr));
-			 email = email.substring(0, 4);
-       this.props.history.push(email);
-     } else {
-			 this.setState({error: "Something wrong with your Email and Password",
-			 errorClass: 'error errorEnabled'});
+
+		 let previousItem = localStorage.getItem('Users');
+		 previousItem = JSON.parse(previousItem);
+		 previousItem = previousItem.filter(function(el) {
+			 return el != null;
+		 });
+
+		 for (var i in previousItem) {
+			 if ( (email === previousItem[i]['username']) && (pass === previousItem[i]['password'])) {
+				 var arr = {'token': true, 'useremail' : previousItem[i]['username'] , 'password' : previousItem[i]['password']};
+	       localStorage.setItem('LoginDetail', JSON.stringify(arr));
+				 email = email.substring(0, 4);
+	       this.props.history.push(email);
+	     } else {
+				 this.setState({error: "Something wrong with your Email and Password",
+				 errorClass: 'error errorEnabled'});
+			 }
 		 }
    }
+
+	 handleSignup() {
+		 if(this.state.signup == false) {
+	     this.setState({
+				 signup: true,
+				 signuptext: "Sign In"
+			 });
+		 } else {
+			 this.setState({
+				 signup: false,
+				 signuptext: "Sing Up"
+			 });
+		 }
+	 }
+
 	 componentDidMount() {
 		 this.setS = setInterval(
         () => this.setState({error: '',
@@ -61,14 +88,16 @@ class LoginPopUP extends React.Component {
   render() {
     return (
 			<Suspense fallback={<div>Loading...</div>}>
+				{this.state.signup ? <SignUp></SignUp> :
 	      <form className="LoginPopUP" onSubmit={this.handleClickLogin} >
 	         <h1> Sign in</h1>
 	         <Input value={this.state.valueEmail} handleChange={this.handleChangeEmail} inputLabel= {this.state.inputName} inputType = {this.state.inputType} inputPlaceholder={'Enter Mail id'} />
 	         <Input value={this.state.valuePass} handleChange={this.handleChangeLogin} inputLabel= {this.state.passwordinputName} inputType = {this.state.passwordinputType} inputPlaceholder={'Enter Password'} />
 	         <input className="button button-blue" type="submit" value="submit"></input>
 					 { this.state.errorClass ? <span className={this.state.errorClass}>{ this.state.error} </span> : ''}
-
-	      </form>
+	       </form>
+		  	}
+				 <button className="button button-blue bottomSpace"  onClick={this.handleSignup.bind(this)}>{this.state.signuptext}</button>
 			</Suspense>
     );
   }
---
+++ src/component/signup/signup.jsx
@@ -0,0 +1,101 @@
+import React from 'react';
+import './signup.css';
+
+class SignUp extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      email: '',
+      pass: '',
+      repeatpass: ''
+    }
+  }
+  handleEmailChange(e) {
+    this.setState({
+      email: e.target.value,
+      userexist: ''
+    });
+  }
+  handlePassChange(e) {
+    this.setState({
+      pass: e.target.value
+    });
+  }
+  handleRepeatPassChange(e) {
+    this.setState({
+      repeatpass: e.target.value
+    });
+  }
+  handleOnSubmit(e) {
+    e.preventDefault();
+    const { email , pass, repeatpass } = this.state;
+    if (pass == repeatpass && pass != '' && repeatpass != ''  && email != '') {
+      this.setState({ passmatch: ""});
+      if( (email != null) && (pass != null) ) {
+        let previousItem = localStorage.getItem('Users');
+        previousItem = JSON.parse(previousItem);
+
+        if(previousItem) {
+          previousItem = previousItem.filter(function(el) {
+            return el != null;
+          });
+          for (var i in previousItem) {
+            if(email != previousItem[i]['username']) {
+              let newarray = [];
+              newarray = [{'username': email, 'password' : pass}];
+              localStorage.setItem('Users', JSON.stringify(newarray.concat(previousItem)));
+              this.setState({ userCreated: 'User created.'})
+            }
+            else {
+              this.setState({userexist: 'User already exists', passmatch: '', userCreated: ''});
+
+            }
+          }
+        } else {
+          let newarray = [];
+          newarray = [{'username': email, 'password' : pass}];
+          localStorage.setItem('Users', JSON.stringify(newarray.concat(previousItem)));
+        }
+
+      }
+    } else {
+      this.setState({ passmatch: "Pass dont match",
+      userCreated: ''});
+
+      return false;
+    }
+    this.setState({
+      pass: '',
+      email: '',
+      repeatpass: ''
+    })
+
+  }
+  render() {
+    return (
+
+      <form className="sing-up-form LoginPopUP" onSubmit={this.handleOnSubmit.bind(this)}>
+        <h1>Sign Up</h1>
+        {this.state.userCreated}
+        <p>Please fill in this form to create an account.</p>
+        <div className="inputField">
+          <label for="email"><b>Email</b></label>
+          <input onChange={this.handleEmailChange.bind(this)} type="text" placeholder="Enter Email" name="email" />
+          {this.state.userexist}
+        </div>
+        <div className="inputField">
+          <label for="psw"><b>Password</b></label>
+          <input  onChange={this.handlePassChange.bind(this)} type="password" placeholder="Enter Password" name="psw" />
+        </div>
+        <div className="inputField">
+          <label for="psw-repeat"><b>Repeat Password</b></label>
+          <input  onChange={this.handleRepeatPassChange.bind(this)} type="password" placeholder="Repeat Password" name="psw-repeat"  />
+          { this.state.passmatch }
+        </div>
+        <input className="button button-blue" type="submit" />
+      </form>
+    );
+  }
+}
+
+export default SignUp;
